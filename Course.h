218c7966a247c368b97511293fcdd99a3918d64b
AndreeTotox <bandicoot611@gmail.com>
2025-09-09 01:11:47 -0300

    Added code to Course Header file (previous commit didn't have it)

--- Course.h
+++ Course.h
@@ -9,14 +9,23 @@ class Course {
     int id;
     std::string name;
     int maxStudents;
-    bool isAvaiable;
+    bool avaiable;
     std::string career;
     std::string teacher;
 
 
     public:
     Course(int id, std::string name, int maxStudents, std::string career, std::string teacher);
+    int getId();
     std::string getName();
+    int getMaxStudents();
+    void setMaxStudents();
+    bool isAvaiable();
+    void setAvaiable(bool status);
+    std::string getCareer();
+    std::string getTeacher();
+    void setTeacher(std::string teacher);
+
 
     ~Course();
 
