1b862a038d0fd4dd2e99a6c7f7d0d9bfbaffc5d7
AndreeTotox <bandicoot611@gmail.com>
2025-09-12 01:40:13 -0300

    Added code for main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,224 @@
 #include <iostream>
 #include <string>
+#include <cctype>
+#include "LinkedList.h"
+#include "Node.h"
+#include "Student.h"
+#include "Course.h"
+
+bool isNumber(std::string& str) {
+    if (str.empty()) {
+        return false;
+    }
+    for (char c : str) {
+        if(!isdigit(c)) {
+            return false;
+        }
+    }
+    return true;
+
+}
+
+std::string askForData(std::string output) {
+    std::string value;
+    std::cout << output;
+    std::getline(std::cin, value);
+    std::cout << "\n";
+    return value;
+}
 
 int main() {
+    bool menu = true;
+    std::string input;
+    int choice;
+
+    StudentList* students = new StudentList();
+    CoursesList* courses = new CoursesList();
+
+    while (menu) {
+        std::cout << "--MENU PRINCIPAL--" << std::endl;
+        std::cout << "1-. Gestion de alumnos" << std::endl;
+        std::cout << "2-. Gestion de cursos" << std::endl;
+        std::cout << "3-. Inscripciones" << std::endl;
+        std::cout << "4-. Notas" << std::endl;
+        std::cout << "5-. Reportes y Consulta" << std::endl;
+        std::cout << "Seleccione su opcion con el numero correspondiente";
+        std::getline(std::cin, input);
+        std::cout << "\n";
+
+        if(isNumber(input)) {
+            choice = stoi(input);
+        } else {
+            std::cout << "Entrada invalida, ingrese un valor correspondiente." << std::endl;
+            continue;
+        }
+
+
+        switch (choice)
+        {
+        case 1:
+            std::cout << "MANEJO DE ALUMNOS\n" << std::endl;
+            studentManagement(input, choice, students);
+            break;
+
+        case 2:
+            std::cout << "MANEJO DE CURSOS\n";
+            break;
+
+        case 3:
+            std::cout << "MANEJO DE INSCRIPCIONES\n";
+            break;
+
+        case 4:
+            std::cout << "MANEJO DE NOTAS\n";
+            break;
+
+        case 5:
+            std::cout << "REPORTES Y CONSULTA\n";
+            break;
+
+        case 6:
+            std::cout << "Saliendo del programa...";
+            menu = false;
+            break;
+        
+        default:
+        std::cout << "Ingrese un numero que este dentro de los parametros";
+            break;
+        }
+    }
+
+    delete courses;
+    delete students;
 
 
     return 0;
+}
+
+void studentManagement(std::string input, int choice, StudentList* students) {
+    std::cout << "----------------------" << std::endl;
+    std::cout << "1-. Crear alumno" << std::endl;
+    std::cout << "2-. Buscar alumno por ID" << std::endl;
+    std::cout << "3-. Buscar alumno por nombre" << std::endl;
+    std::cout << "4-. Eliminar alumno" << std::endl;
+    std::cout << "Indique su eleccion con el numero correspondiente" << std::endl;
+    std::cout << "Cualquier caso fuera de lugar lo llevara devuelta al menu principal: ";
+    std::getline(std::cin, input);
+    std::cout << "\n";
+
+    if(isNumber(input)) {
+        choice = stoi(input);
+    } else {
+        std::cout << "Regresando..." << std::endl;
+        return;
+    }
+
+    switch (choice)
+    {
+    case 1: {
+        int id;
+        std::cout << "Ingrese el ID del alumno a agregar: ";
+        std::getline(std::cin, input);
+        std::cout << "\n";
+        if (isNumber(input)){
+            id = stoi(input);
+        } else {
+            std::cout << "FORMATO INVALIDO! Regresando..." << std::endl;
+            return;
+        }
+        std::string name = askForData("Ingrese el nombre del alumno a agregar: ");
+        std::string lastName = askForData("Ingrese el apellido del alumno a agregar: ");
+        std::string career = askForData("Ingrese la carrera del alumno a agregar: ");
+        std::string day = askForData("Ingrese el dia del mes que ingreso a la universidad: ");
+        std::string month = askForData("Ingrese el mes que ingreso a la universidad: ");
+        std::string year = askForData("Ingrese el año del alumno a agregar: ");
+        std::string entryDate = day + "/" + month + "/" + year;
+
+        Student* s = new Student(id, name, lastName, career, entryDate);
+        students->addStudent(s);
+        break;
+    }
+
+    case 2: {
+        int id;
+
+        std::cout << "Ingrese el ID del alumno a buscar: ";
+        std::getline(std::cin, input);
+        std::cout << "\n";
+        if (isNumber(input)){
+            id = stoi(input);
+        } else {
+            std::cout << "FORMATO INVALIDO! Regresando..." << std::endl;
+            return;
+        }
+        students->search(id);
+        break;
+
+    }
+
+    case 3: {
+        std::string name = askForData("Ingrese el nombre del alumno a buscar: ");
+        students->search(name);
+        break;
+    }
+
+    case 4: {
+        int id;
+
+        std::cout << "Ingrese el ID del alumno a eliminar: ";
+        std::getline(std::cin, input);
+        std::cout << "\n";
+        if (isNumber(input)){
+            id = stoi(input);
+        } else {
+            std::cout << "FORMATO INVALIDO! Regresando..." << std::endl;
+            return;
+        }
+        if (students->remove(id)){
+            std::cout << "El alumno a sido removido exitosamente!" << std::endl;
+        } else {
+            std::cout << "No se pudo remover al alumno debido a que el ID entregado no es valido o no existe" << std::endl;
+        }
+        break;
+
+    }
+    
+    default:
+        std::cout << "Regresando...";
+        return;
+    }
+
+
+
+}
+
+void courseManagement(std::string input, int choice, CoursesList* courses) {
+    std::cout << "----------------------" << std::endl;
+    std::cout << "1-. Crear curso" << std::endl;
+    std::cout << "2-. Buscar curso por ID" << std::endl;
+    std::cout << "3-. Buscar curso por nombre" << std::endl;
+    std::cout << "4-. Eliminar alumno" << std::endl;
+    std::cout << "Indique su eleccion con el numero correspondiente" << std::endl;
+    std::cout << "Cualquier caso fuera de lugar lo llevara al menu principal: ";
+    std::getline(std::cin, input);
+    std::cout << "\n";
+
+    if(isNumber(input)) {
+        choice = stoi(input);
+    } else {
+        std::cout << "Regresando..." << std::endl;
+        return;
+    }
+
+    switch (choice)
+    {
+    case 1:
+        break;
+    
+    default:
+        break;
+    }
+
+
+
 }
