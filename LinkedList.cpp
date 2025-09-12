4fa40e8d32ed0a41fa83bad3403279b0bfdca143
AndreeTotox <bandicoot611@gmail.com>
2025-09-12 18:25:19 -0300

    Added code to LinkedList Header and C++ Files. Added class EnrollmentList and GradeList to header and methods for EnrollmentList in C++ file

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -4,20 +4,20 @@
 #include "Course.h"
 
 StudentList::StudentList() {
-    this->head = nullptr;
+    head = nullptr;
 }
 
 NodeStudent* StudentList::getHead() {
-    return this->head;
+    return head;
 }
 
 void StudentList::addStudent(Student* student) {
     NodeStudent* studentNode = new NodeStudent(student);
-    if (this->isEmpty()) {
-        this->head = studentNode;
+    if (isEmpty()) {
+        head = studentNode;
         return;
     }
-    NodeStudent* cursor = this->head;
+    NodeStudent* cursor = head;
     while (cursor->getNext() != nullptr)
     {
         cursor = cursor->getNext();
@@ -27,8 +27,8 @@ void StudentList::addStudent(Student* student) {
     
 }
 
-bool StudentList::remove(int id) {
-    if (this->isEmpty()) { 
+bool StudentList::remove(int id, EnrollmentList* enrolls) {
+    if (isEmpty()) { 
         return false;
     }
     NodeStudent* current = head;
@@ -40,6 +40,7 @@ bool StudentList::remove(int id) {
             } else {
                 previous->setNext(current->getNext());
             }
+            enrolls->removeStudent(current->getValue()->getId());
 
             delete current;
             return true;
@@ -52,12 +53,85 @@ bool StudentList::remove(int id) {
     return false;
 }
 
+void StudentList::search(std::string name) {
+    NodeStudent* current = head;
+    if (isEmpty()){
+        std::cout << "No hay alumnos registrados en la lista" << std::endl;
+        return;
+    } else {
+        while (current != nullptr) {
+            if (current->getValue()->getName() == name) {
+                current->getValue()->showAll();
+            }
+            current = current->getNext();
+        }
+    }
+
+}
+
+void StudentList::search(int id) {
+    NodeStudent* current = head;
+    if (isEmpty()){
+        std::cout << "No hay alumnos registrados en la lista" << std::endl;
+        return;
+    } else {
+        while (current != nullptr) {
+            if (current->getValue()->getId() == id) {
+                current->getValue()->showAll();
+                return;
+            }
+            current = current->getNext();
+        }
+    }
+
+}
+
+bool StudentList::verifyId(int id) {
+
+    NodeStudent* cursor = head;
+    while (cursor != nullptr) {
+        if (cursor->getValue()->getId() == id) {
+            return true;
+        }
+        cursor = cursor->getNext();
+    }
+
+    return false;
+
+}
+
+Student* StudentList::getStudent(int id) {
+    NodeStudent* cursor = head;
+    while (cursor != nullptr) {
+        if (cursor->getValue()->getId() == id) {
+            return cursor->getValue();
+        }
+        cursor = cursor->getNext();
+    }
+
+    return nullptr;
+
+}
+
 
 bool StudentList::isEmpty() {
-    if (this->head == nullptr) {
+    if (head == nullptr) {
         return true;
     }
     return false;
+}
+
+void StudentList::showInfo() {
+    if (isEmpty()) {
+        std::cout << "No hay estudiantes registrados en el sistema" << std::endl;
+        return;
+    }
+
+    NodeStudent* cursor = head;
+    while (cursor != nullptr) {
+        cursor->getValue()->showInfo();
+        cursor = cursor->getNext();
+    }
 }
 
 StudentList::~StudentList() {
@@ -68,10 +142,281 @@ StudentList::~StudentList() {
         Student* valueTemp = current->getValue();
 
         current = current->getNext();
-        delete temp;
-
         delete valueTemp;
+
+        delete temp;
     }
 
     head = nullptr;
+}
+
+CoursesList::CoursesList() {
+    head = nullptr;
+}
+
+NodeCourse* CoursesList::getHead() {
+    return head;
+}
+
+void CoursesList::addCourse(Course* data) {
+    NodeCourse* courseNode = new NodeCourse(data);
+    if (isEmpty()) {
+        head = courseNode;
+        return;
+    }
+    NodeCourse* cursor = head;
+    while (cursor->getNext() != nullptr)
+    {
+        cursor = cursor->getNext();
+    }
+
+    cursor->setNext(courseNode);
+
+}
+
+bool CoursesList::remove(int id, EnrollmentList* enrolls) {
+    if (isEmpty()) { 
+        return false;
+    }
+    NodeCourse* current = head;
+    NodeCourse* previous = nullptr;
+    while (current != nullptr) {
+        if (current->getValue()->getId() == id) {
+            if (previous == nullptr) {
+                head = current->getNext();
+            } else {
+                previous->setNext(current->getNext());
+            }
+            enrolls->removeCourse(current->getValue()->getId());
+
+            delete current;
+            return true;
+        }
+        previous = current;
+        current = current->getNext();
+
+    }
+
+    return false;
+}
+
+void CoursesList::search(std::string name) {
+    NodeCourse* current = head;
+    if (isEmpty()){
+        std::cout << "No hay cursos registrados en la lista" << std::endl;
+        return;
+    } else {
+        while (current != nullptr) {
+            if (current->getValue()->getName() == name) {
+                current->getValue()->show();
+            }
+            current = current->getNext();
+        }
+    }
+
+}
+
+void CoursesList::search(int id) {
+    NodeCourse* current = head;
+    if (isEmpty()){
+        std::cout << "No hay cursos registrados en la lista" << std::endl;
+        return;
+    } else {
+        while (current != nullptr) {
+            if (current->getValue()->getId() == id) {
+                current->getValue()->show();
+                return;
+            }
+            current = current->getNext();
+        }
+    }
+
+}
+
+Course* CoursesList::getCourse(int id) {
+    NodeCourse* cursor = head;
+    while (cursor != nullptr) {
+        if (cursor->getValue()->getId() == id) {
+            return cursor->getValue();
+        }
+        cursor = cursor->getNext();
+    }
+
+    return nullptr;
+
+}
+
+bool CoursesList::verifyId(int id) {
+
+    NodeCourse* cursor = head;
+    while (cursor != nullptr) {
+        if (cursor->getValue()->getId() == id) {
+            return true;
+        }
+        cursor = cursor->getNext();
+    }
+
+    return false;
+
+}
+
+void CoursesList::showInfo() {
+    if (isEmpty()) {
+        std::cout << "No hay cursos registrados en el sistema" << std::endl;
+        return;
+    }
+
+    NodeCourse* cursor = head;
+    while (cursor != nullptr) {
+        cursor->getValue()->show();
+        cursor = cursor->getNext();
+    }
+}
+
+
+bool CoursesList::isEmpty() {
+    if (head == nullptr) {
+        return true;
+    }
+    return false;
+}
+
+CoursesList::~CoursesList() {
+    NodeCourse* current = head;
+    while (current != nullptr) {
+        NodeCourse* temp = current;
+
+        Course* valueTemp = current->getValue();
+
+        current = current->getNext();
+        delete valueTemp;
+
+        delete temp;
+    }
+
+    head = nullptr;
+
+}
+
+EnrollmentList::EnrollmentList() {
+    head = nullptr;
+}
+
+NodeSigned* EnrollmentList::getHead() {
+    return head;
+}
+
+void EnrollmentList::addEnroll(Student* s, Course* c) {
+    NodeSigned* enrollNode = new NodeSigned(s,c);
+    enrollNode->getCourse()->addSigned();
+    if (isEmpty()) {
+        head = enrollNode;
+        return;
+    }
+    NodeSigned* cursor = head;
+    while (cursor->getNext() != nullptr)
+    {
+        cursor = cursor->getNext();
+    }
+
+    cursor->setNext(enrollNode);
+    
+
+}
+
+bool EnrollmentList::remove(int idS, int idC) {
+    if (isEmpty()) { 
+        return false;
+    }
+    NodeSigned* current = head;
+    NodeSigned* previous = nullptr;
+    while (current != nullptr) {
+        if (current->getStudent()->getId() == idS && current->getCourse()->getId() == idC) {
+            if (previous == nullptr) {
+                head = current->getNext();
+            } else {
+                previous->setNext(current->getNext());
+            }
+            current->getCourse()->removeSigned();
+
+            delete current;
+            return true;
+        }
+        previous = current;
+        current = current->getNext();
+
+    }
+
+    return false;
+
+}
+
+void EnrollmentList::removeCourse(int id) {
+    if (isEmpty()) { 
+        return;
+    }
+    NodeSigned* current = head;
+    NodeSigned* previous = nullptr;
+    while (current != nullptr) {
+        if (current->getCourse()->getId() == id) {
+            NodeSigned* aux = current;
+            if (previous == nullptr) {
+                head = current->getNext();
+            } else {
+                previous->setNext(current->getNext());
+            }
+            
+            current = current->getNext();
+            delete aux;
+        } else {
+            previous = current;
+            current = current->getNext();
+        }
+    }
+
+}
+
+void EnrollmentList::removeStudent(int id) {
+    if (isEmpty()) { 
+        return;
+    }
+    NodeSigned* current = head;
+    NodeSigned* previous = nullptr;
+    while (current != nullptr) {
+        if (current->getStudent()->getId() == id) {
+            NodeSigned* aux = current;
+            if (previous == nullptr) {
+                head = current->getNext();
+            } else {
+                previous->setNext(current->getNext());
+            }
+            current->getCourse()->removeSigned();
+            
+            current = current->getNext();
+            delete aux;
+        } else {
+            previous = current;
+            current = current->getNext();
+        }
+    }
+
+}
+
+bool EnrollmentList::isEmpty() {
+    if (head == nullptr) {
+        return true;
+    }
+    return false;
+}
+
+EnrollmentList::~EnrollmentList() {
+    NodeSigned* current = head;
+    while (current != nullptr) {
+        NodeSigned* temp = current;
+        current = current->getNext();
+        delete temp;
+    }
+
+    head = nullptr;
+
 }
--- LinkedList.h
+++ LinkedList.h
@@ -14,7 +14,10 @@ class StudentList {
     void addStudent(Student* student);
     void search(std::string name);
     void search(int id);
-    bool remove(int id);
+    bool remove(int id, EnrollmentList* enrolls);
+    bool verifyId(int id);
+    Student* getStudent(int id);
+    void showInfo();
     bool isEmpty();
 
     ~StudentList();
@@ -29,8 +32,13 @@ class CoursesList {
     public:
     CoursesList();
     NodeCourse* getHead();
-    bool addCourse(Course* head);
-    bool remove(int id);
+    void addCourse(Course* head);
+    void search(std::string name);
+    void search(int id);
+    bool remove(int id, EnrollmentList* enrolls);
+    bool verifyId(int id);
+    Course* getCourse(int id);
+    void showInfo();
     bool isEmpty();
 
 
@@ -38,6 +46,32 @@ class CoursesList {
 
 };
 
+class EnrollmentList {
+
+    private:
+    NodeSigned* head;
+
+    public:
+    EnrollmentList();
+    NodeSigned* getHead();
+    void addEnroll(Student* s, Course* c);
+    bool remove(int idS, int idC);
+    void removeStudent(int id);
+    void removeCourse(int id);
+    bool isEmpty();
+
+    ~EnrollmentList();
+};
+
 class GradesList {
+
+    private:
+    NodeGrades* head;
+
+    public:
+    GradesList();
+    int size();
+
+    ~GradesList();
 
 };
