400518c6876caab5a3395d0c06be56980e3b1d1a
AndreeTotox <bandicoot611@gmail.com>
2025-09-12 22:13:20 -0300

    Finished code for Node Header and C++ Files.

--- Node.cpp
+++ Node.cpp
@@ -7,6 +7,100 @@ NodeStudent::NodeStudent(Student* data){
     this->next = nullptr;
 }
 
+Student* NodeStudent::getValue() {
+    return this->value;
+}
+
+void NodeStudent::setValue(Student* value) {
+    this->value = value;
+
+}
+
+NodeStudent* NodeStudent::getNext() {
+    return this->next;
+}
+
 void NodeStudent::setNext(NodeStudent* next) {
     this->next = next;
+}
+
+NodeStudent::~NodeStudent() {
+
+}
+
+NodeCourse::NodeCourse(Course* data) {
+    this->value = data;
+    this->next = nullptr;
+}
+
+Course* NodeCourse::getValue() {
+    return this->value;
+}
+
+void NodeCourse::setValue(Course* data) {
+    this->value = data;
+}
+
+NodeCourse* NodeCourse::getNext() {
+    return this->next;
+}
+
+void NodeCourse::setNext(NodeCourse* next) {
+    this->next = next;
+}
+
+NodeCourse::~NodeCourse() {
+    //This destroys!
+}
+
+NodeSigned::NodeSigned(Student* student, Course* course) {
+    this->student = student;
+    this->course = course;
+    grades = new GradesList();
+    next = nullptr;
+}
+
+Student* NodeSigned::getStudent() {
+    return student;
+}
+
+Course* NodeSigned::getCourse() {
+    return course;
+}
+
+NodeSigned* NodeSigned::getNext() {
+    return this->next;
+}
+
+void NodeSigned::setNext(NodeSigned* next) {
+    this->next = next;
+}
+
+NodeSigned::~NodeSigned() {
+    delete grades;
+}
+
+NodeGrades::NodeGrades(double data) {
+    value = data;
+    next = nullptr;
+}
+
+double NodeGrades::getValue() {
+    return value;
+}
+
+void NodeGrades::setValue(double data) {
+    value = data;
+}
+
+NodeGrades* NodeGrades::getNext() {
+    return next;
+}
+
+void NodeGrades::setNext(NodeGrades* next) {
+    this->next = next;
+}
+
+NodeGrades::~NodeGrades() {
+    
 }
--- Node.h
+++ Node.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Course.h"
 #include "Student.h"
+#include "LinkedList.h"
 
 
 class NodeStudent {
@@ -14,15 +15,15 @@ class NodeStudent {
     NodeStudent(Student* data);
     Student* getValue();
 
-    void setValue(Student& data);
+    void setValue(Student* data);
 
     NodeStudent* getNext();
 
     void setNext(NodeStudent* next);
 
-    ~NodeStudent() {
+    ~NodeStudent();
         //This is a destructor
-    }
+    
 
 
 };
@@ -35,23 +36,58 @@ class NodeCourse {
 
     public:
 
-    NodeCourse(Course& data);
+    NodeCourse(Course* data);
     Course* getValue();
 
-    void setValue(Course& data);
+    void setValue(Course* data);
 
     NodeCourse* getNext();
 
     void setNext(NodeCourse* next);
 
-    ~NodeCourse() {
+    ~NodeCourse();
         //This is a destructor too
-    }
+    
 
+
+};
+
+class NodeGrades {
+
+    private:
+    double value;
+    NodeGrades* next;
+
+    public:
+    NodeGrades(double data);
+    double getValue();
+    void setValue(double data);
+    NodeGrades* getNext();
+    void setNext(NodeGrades* next);
+
+    ~NodeGrades();
 
 };
 
 
 class NodeSigned {
+
+    private:
+    Student* student;
+    Course* course;
+    GradesList* grades;
+    NodeSigned* next;
+
+    public:
+    NodeSigned(Student* student, Course* course);
+    Course* getCourse();
+    Student* getStudent();
+    NodeSigned* getNext();
+    void setNext(NodeSigned* next);
+    GradesList* getGrades();
+    void addGrade(double grade);
+
+    ~NodeSigned();
+
 
 };
